904c76f7682bc0fd0eb5c94444e48aeed53c9947
Nycolas Padilha <763npp@gmail.com>
2024-03-25 09:11:17 -0300

    season scraper

---
+++ data_scraping/scraper.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+from typing import List, Dict, Any
+
+
+def fetch():
+    url = 'https://myanimelist.net/anime/season'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        return 'Error fetching page'
+
+
+def anime_formatter(anime: Any):
+    img = anime.find('div', {"class": 'image'}).a.img
+    img_src = img['src'] if img and 'src' in img.attrs else img['data-src']
+
+    return {
+        'title': anime.find('span', {"class": 'js-title'}).text,
+        'start_date': anime.find('span', {"class": 'js-start_date'}).text[4:],
+        'url': anime.find('div', {"class": 'image'}).a['href'],
+        'image': img_src,
+    }
+
+
+def new_season():
+    soup = BeautifulSoup(fetch(), 'html.parser')
+
+    season: List[Dict[str, Any]] = []
+
+    for anime in soup.find_all(
+        'div',
+        {"class": 'js-anime-category-producer seasonal-anime js-seasonal-anime js-anime-type-all js-anime-type-1'}
+    ):
+        gay = bool(anime.find('span', {"class": 'genre'}, text={'Boys Love'}))
+
+        if not gay:
+            a = anime_formatter(anime)
+            season.append(a)
+
+    return season
+
+
+print(new_season())
