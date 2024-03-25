7dd040ab556fd23b578c48620511b74b1f0e7814
Nycolas Padilha <763npp@gmail.com>
2024-03-25 10:27:08 -0300

    add image

--- list.py
+++ list.py
@@ -7,6 +7,7 @@ sorted_animes = sorted(animes, key=lambda anime: anime['start_date'])
 with open('anime_list.txt', 'w') as file:
     for anime in sorted_animes:
         file.write(anime['start_date'] + '\n')
+        file.write(anime['image'] + '\n')
         file.write(anime['title'] + '\n')
         file.write(anime['url'] + '\n')
         file.write('\n')
